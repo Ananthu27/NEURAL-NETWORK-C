29de194125e7c04eab2fd496bf86a2dc09b1aa4b
Ananthu <kma270999@gmail.com>
2020-05-02 16:32:32 +0530

    added traspose and new overloads to add an dot in numpy

--- nn.cpp
+++ nn.cpp
@@ -8,16 +8,21 @@ int main()
 {
     numpy np;
 
-    vector<float> inputs = {1, 2, 3, 2.5};
+    vector<vector<float>> inputs = {
+        {1, 2, 3, 2.5},
+        {2.0, 5, -1, 2},
+        {-1.5, 2.7, 3.3, -0.8}};
+
     vector<vector<float>> weights = {
         {0.2, 0.8, -0.5, 1.0},
         {0.5, -0.91, 0.26, -0.5},
         {-0.26, -0.27, 0.17, 0.87}};
+
     vector<float> biases = {2, 3, 0.5};
 
-    vector<float>
-        layer_outputs = np.add(np.dot(weights, inputs), biases);
+    vector<vector<float>> layer_outputs = np.add(np.dot(inputs, np.transpose(weights)), biases);
 
     np.display(layer_outputs);
+
     return 0;
 }
--- numpy.h
+++ numpy.h
@@ -7,8 +7,14 @@ class numpy
 {
 public:
     vector<float> add(vector<float>, vector<float>);
+    vector<vector<float>> add(vector<vector<float>>, vector<float>);
+
     float dot(vector<float>, vector<float>);
     vector<float> dot(vector<vector<float>>, vector<float>);
+    vector<vector<float>> dot(vector<vector<float>> a, vector<vector<float>> b);
+
+    vector<vector<float>> transpose(vector<vector<float>>);
+
     void display(vector<float>);
     void display(vector<vector<float>>);
 };
@@ -20,6 +26,19 @@ vector<float> numpy::add(vector<float> a, vector<float> b)
     {
         for (int i = 0; i < a.size(); i++)
             result.push_back(a[i] + b[i]);
+    }
+    return result;
+}
+
+vector<vector<float>> numpy::add(vector<vector<float>> a, vector<float> b)
+{
+    vector<vector<float>> result;
+    if (a[0].size() == b.size())
+    {
+        for (int i = 0; i < a.size(); i++)
+        {
+            result.push_back(add(a[i], b));
+        }
     }
     return result;
 }
@@ -46,6 +65,33 @@ vector<float> numpy::dot(vector<vector<float>> a, vector<float> b)
         }
     }
     return result;
+}
+
+vector<vector<float>> numpy::dot(vector<vector<float>> a, vector<vector<float>> b)
+{
+    vector<vector<float>> result;
+    if (a[0].size() == b.size())
+    {
+        result = vector<vector<float>>(a.size(), vector<float>(b[0].size(), 0));
+        for (int i = 0; i < a.size(); i++)
+            for (int j = 0; j < b[0].size(); j++)
+                for (int k = 0; k < b.size(); k++)
+                    result[i][j] += a[i][k] * b[k][j];
+    }
+    return result;
+}
+
+vector<vector<float>> numpy::transpose(vector<vector<float>> a)
+{
+    vector<vector<float>> new_a = vector<vector<float>>(a[0].size(), vector<float>(a.size(), 0));
+    for (int i = 0; i < a.size(); i++)
+    {
+        for (int j = 0; j < a[0].size(); j++)
+        {
+            new_a[j][i] = a[i][j];
+        }
+    }
+    return new_a;
 }
 
 void numpy::display(vector<float> a)
